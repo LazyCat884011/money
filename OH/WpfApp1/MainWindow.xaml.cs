4d05c673c8f5ad15d34bc7243da6223f5211a7c6
LazyCat884011 <37174613+LazyCat884011@users.noreply.github.com>
2018-06-14 16:58:33 +0800

    OH 按ENTER 計算總額

--- OH/WpfApp1/MainWindow.xaml.cs
+++ OH/WpfApp1/MainWindow.xaml.cs
@@ -34,38 +34,38 @@ namespace WpfApp1
             // 分析
             foreach (string line in lines)
             {
-                // 用 | 符號隔開
+                // 隔開
                 string[] parts = line.Split('|');
 
-                // 建立事件
+                
                 price price = new price();
 
-                //讀取不同部份
+                //讀取
                 price.date.Text = parts[0];
                 price.TaskName.Text = parts[1];
                 price.TaskPrice.Text = parts[2];
 
-                // 放到清單
+                //增加項目
                 TaskList.Children.Add(price);
             }
         }
 
        private void Window_KeyDown(object sender, KeyEventArgs e)
         {
-            //建立一個空數字
+            
             int AddPrice = 0;
 
-            //按ENTER時會計算總數
+            //按enter時會計算
             if (e.Key == Key.Return)
             {
-                // 計算每個項目
+                
                 foreach (price item in TaskList.Children)
                 {
-                    //價格相加
+                    
                     AddPrice += item.itemPriceValue;
                 }
 
-                //顯示總額
+                //顯示總價
                 totalPrice.Text = AddPrice.ToString();
             }
         }
@@ -75,16 +75,16 @@ namespace WpfApp1
             // 新增串列裝每個項目的文字
             List<string> datas = new List<string>();
 
-            // 轉換每個項目
+            // 轉換項目
             foreach (price price in TaskList.Children)
             {
-                //設置一個空字串
+                //設置一個字串
                 string data = "";
 
-                //每一種資料以"|"區隔加入data字串
+                //每一種資料區隔加入字串
                 data += price.date.Text + "|" + price.TaskName.Text + "|" + price.TaskPrice.Text;
 
-                //加入Datas的陣列
+                
                 datas.Add(data);
             }
 
@@ -94,10 +94,10 @@ namespace WpfApp1
 
         private void addTask_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            // 產生項目
+            // 產生
             price price = new price();
 
-            //放至清單中
+            //放到清單
             TaskList.Children.Add(price);
         }
     }
